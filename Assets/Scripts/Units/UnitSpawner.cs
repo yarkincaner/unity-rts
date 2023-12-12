7ca369208db6e0e37ca47a4234a975a20c9cb370
Ali Yarkin CANER <yarkincaner@gmail.com>
2023-12-12 21:07:28 +0300

    feat: add spawnable villager

--- Assets/Scripts/Units/UnitSpawner.cs
+++ Assets/Scripts/Units/UnitSpawner.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 public class UnitSpawner : MonoBehaviour
 {
     [SerializeField] GameObject soldierPrefab;
+    [SerializeField] GameObject villagerPrefab;
+
     public void SpawnSoldier()
     {
         PhotonNetwork.Instantiate(soldierPrefab.name, new Vector3(43, 0, 50), Quaternion.identity, 0, null);
+    }
+
+    public void SpawnVillager()
+    {
+        PhotonNetwork.Instantiate(villagerPrefab.name, new Vector3(43, 0, 50), Quaternion.identity, 0, null);
     }
 }
